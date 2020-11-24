3f7f5bc3aa97be749856bf38dccafa35a06276b7
WhiteWolf6 <54216526+WhiteWolf6@users.noreply.github.com>
2020-11-24 07:18:01 +0300

    2nd commit

--- categories/src/app/managers/managers.component.ts
+++ categories/src/app/managers/managers.component.ts
@@ -26,6 +26,9 @@ export interface employees {
   patronymic: string;  
 }
 
+export interface newObj {
+  [counts:number]: number;
+}
 
 @Component({
   selector: 'app-managers',
@@ -37,7 +40,8 @@ export class ManagersComponent implements OnInit {
 
   displayedColumns: string[] = ['lname', 'fname', 'patronymic', '1', '2','3','4','5',
   '6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24',
-  '25','26','27','28','29','30'];
+  '25','26','27','28','29', '30','w','lastCount'];
+  
   displayedColumns2: string[] = ['lname']
   dataSource: any;
   dataSource2: any;
@@ -91,87 +95,60 @@ export class ManagersComponent implements OnInit {
       data: {
       countt: this.count,
       countt2: this.count2,
+      data1: this.pair,
+      data2: this.pair2
       }, 
-    });
+    }); console.log(this.pair)
     dialogRef.afterClosed().subscribe((result) => {
     //  if (result)
-        console.log(2+2*2); 
-
+      //  console.log(2+2*2); 
      // }
     });   
   }
-  
-  n: number = 0;
+
   count: number = 0;
- radioChange1($event : MatRadioChange):void{
-  if($event){
-    this.count++;
-   }
-}
-check: boolean = false;
-check2: boolean = false;
-radioChange3(completed: boolean){
-  if(completed == true){
-this.count++;
-  } else if(completed == false){this.count--;}
-}
-radioChange4(completed2: boolean){
-  if(completed2 == true){
-    this.count2++;
-      } else if(completed2 == false){this.count2--;}
-}
-
-count2: number = 0;
-
-radioChange2($event : MatRadioChange):void{
-  if($event){
-   this.count2++;
+  count2: number = 0;
+  check: boolean = false;
+  check2: boolean = false;
+  obj: newObj[];
+  ex: {}
+  pair = {};
+  pair2 = {};
+  
+  radioChange3(completed: boolean,element:any){  
+   // console.log(element); 
+    if(this.pair[element.idEmployees] === undefined){
+    this.pair[element.idEmployees] = 0;}
+      if(completed == true){
+      // this.count=0;
+      this.count++;
+      this.pair[element.idEmployees]+=1;
+    
+      } else if(completed == false)
+      {
+        this.count--;
+        this.pair[element.idEmployees]-=1;
+      }
+     // console.log('count=',this.count);
+      console.log(this.pair); 
   }
- }
-
-  mark = ["больничный","прогул"];
-  mark2 = ["больничный","прогул"];
-  mark3 = ["больничный","прогул"];
-  mark4 = ["больничный","прогул"];
-  mark5 = ["больничный","прогул"];
-  mark6 = ["больничный","прогул"];
-  mark7 = ["больничный","прогул"];
-  mark8 = ["больничный","прогул"];
-  mark9 = ["больничный","прогул"];
-  mark10 = ["больничный","прогул"];
-  mark11 = ["больничный","прогул"];
-  mark12 = ["больничный","прогул"];
-  mark13 = ["больничный","прогул"];
-  mark14 = ["больничный","прогул"];
-  mark15 = ["больничный","прогул"];
-  mark16 = ["больничный","прогул"];
-  mark17 = ["больничный","прогул"];
-  mark18 = ["больничный","прогул"];
-  mark19 = ["больничный","прогул"];
-  mark20 = ["больничный","прогул"];
-  mark21 = ["больничный","прогул"];
-  mark22 = ["больничный","прогул"];
-  mark23 = ["больничный","прогул"];
-  mark24 = ["больничный","прогул"];
-  mark25 = ["больничный","прогул"];
-  mark26 = ["больничный","прогул"];
-  mark27 = ["больничный","прогул"];
-  mark28 = ["больничный","прогул"];
-  mark29 = ["больничный","прогул"];
-  mark30 = ["больничный","прогул"];
-
-  setValue(element){
-    console.log(element.mark);
-     element = element.mark;
-  }
-
-  setValue2(element){
-    console.log(element.mark2);
-   // let v = element.mark;
-  }
-  setValue3(element){
-    console.log(element.mark3);
-   // let v = element.mark;
+  
+  radioChange4(completed2: boolean,element:any){
+    if(this.pair2[element.idEmployees] === undefined){
+      this.pair2[element.idEmployees] = 0;}
+    if(completed2 == true){
+      // this.count=0;
+     this.count2++;
+     this.pair2[element.idEmployees]+=1;
+     
+     } else if(completed2 == false)
+       {
+         this.count2--;
+         this.pair2[element.idEmployees]-=1;
+ 
+       }
+     //  console.log('count2=',this.count2);
+       console.log(this.pair2);
   }
 
   goBack(): void {
@@ -179,3 +156,17 @@ radioChange2($event : MatRadioChange):void{
   }
 
 }
+/*this.pair[element.idEmployees] = 0;
+
+    if(completed == true){
+     this.count++;
+      this.pair[element.idEmployees] += 1;
+
+    } else if(completed == false)
+      {
+        this.count--;
+        this.pair[element.idEmployees] -=1;
+
+      }*/
+
+     // this.days.forEach(n =>{
